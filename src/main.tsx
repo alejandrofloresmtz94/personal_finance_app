178d0f2fea5045d615419fc9ff14029ac0f3110f
Alejandro Flores Martinez <alejandro.flores@finvero.com>
2024-12-28 03:29:39 -0600

    added firebase basic configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import { FirebaseAppProvider } from 'reactfire';
+import './index.css'
+
+const {
+  REACT_APP_FIREBASE_API_KEY: apiKey,
+  REACT_APP_FIREBASE_AUTH_DOMAIN: authDomain,
+  REACT_APP_FIREBASE_PROJECT_ID: projectId,
+  REACT_APP_FIREBASE_STORAGE_BUCKET: storageBucket,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID: messagingSenderId,
+  REACT_APP_FIREBASE_APP_ID: appId,
+} = process.env;
+
+const firebaseConfig = {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+};
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
+  <StrictMode> 
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <App />
+    </FirebaseAppProvider>
   </StrictMode>,
 )
