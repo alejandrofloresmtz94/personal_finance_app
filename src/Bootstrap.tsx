7db71414a341b71bc66f50a59d79b374143afccf
Alejandro Flores <alejandrofloresmtz94@gmail.com}>
2024-12-30 13:07:21 -0600

    deleted emulator connection

--- src/Bootstrap.tsx
+++ src/Bootstrap.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { connectAuthEmulator, getAuth } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { AuthProvider, useFirebaseApp } from "reactfire";
 import { BrowserRouter } from "react-router-dom";
 import { PrimeReactProvider } from 'primereact/api';
@@ -8,13 +8,6 @@ import App from "./App";
 const Bootstrap: React.FC = () => {
     const app = useFirebaseApp();
     const auth = getAuth(app);
-
-    // Check for dev/test mode however your app tracks that.
-    // `process.env.NODE_ENV` is a common React pattern
-    if (process.env.NODE_ENV !== 'production') {
-        // Set up emulators
-        connectAuthEmulator(auth, 'http://192.168.0.134:9099');
-    }
 
     return (
         <AuthProvider sdk={auth}>
